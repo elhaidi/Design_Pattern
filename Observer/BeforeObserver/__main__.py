5e773d092299ec7b67b2248a29dab81e04f695b2
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2019-08-29 22:52:02 +0100

    example without the observer

---
+++ Observer/BeforeObserver/__main__.py
@@ -0,0 +1,10 @@
+from kpi_data import KPI_Data
+
+# REport in current KPI values
+for kpi in KPI_Data:
+    if kpi.name == 'open':
+        print("Current open ticket {}".format(kpi.value))
+    elif kpi.name == "new":
+        print('New tickets for today is {}'.format(kpi.value))
+    elif kpi.name == 'closed':
+        print('the ticket closed for today is {}'.format(kpi.value))

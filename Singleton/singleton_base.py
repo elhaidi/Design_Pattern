76fbf8294481aa270fd1e317abe76ee5580840c9
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2019-09-03 21:34:49 +0100

    adding the base singleton example

---
+++ Singleton/singleton_base.py
@@ -0,0 +1,9 @@
+class Singleton:
+    _instances={} # dict([cls,instances])
+
+    def __new__(cls,*args,**kwargs):
+        if cls not in cls._instances:
+            instance = super().__new__(cls)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+    
